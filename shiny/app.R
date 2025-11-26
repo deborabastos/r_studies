67f57441506b74293b539822c187509a1e03c114
deborabastos <deborabastos14@gmail.com>
2025-11-25 23:00:05 -0300

    atualização em shiny

--- shiny/app.R
+++ shiny/app.R
@@ -1,8 +1,24 @@
 library(shiny)
 
-ui <- fluidPage("Hello World")
+ui <- fluidPage(
+  titlePanel("Página"),
+  selectInput(
+    inputId = "select",
+    label = "Select",
+    choices = c("1", "2", "3"),
+    selected = "1"
+  ),
+  textOutput("text")
+)
 
-server <- function(input, output, session) {}
+server <- function(input, output) {
+  output$text <- renderText({
+    # para a renderização e deixa verificar variáveis no terminal - input$select
+    # Para o browser() continuar a rodar o código: c
+    # Para sair do browser(): Q ou f???
+    # browser()
+    paste("O número é", input$select)
+  })
+}
 
-shinyApp(ui, server)
-
+shinyApp(ui = ui, server = server, options = list(launch.browser = FALSE, port = 4242)) # nolint: line_length_linter.
---
+++ shiny/deploy.R
@@ -0,0 +1,12 @@
+rsconnect::deployApp(
+  appName = "meuApp",
+  account = "meuuser"
+)
+
+# Se tiver dentro do Golem, usa a função deploy
+deploy <- function() {
+  rsconnect::deployApp(
+    appName = "meuApp",
+    account = "meuuser"
+  )
+}
