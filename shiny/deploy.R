47bff4f88609990d2812702d32690406a4119c6d
deborabastos <deborabastos14@gmail.com>
2025-12-03 21:49:25 -0300

    aprendendo shiny

--- shiny/deploy.R
+++ shiny/deploy.R
@@ -17,7 +17,7 @@ deploy <- function() {
 
 # Para publicar, no console:
 #library(rsconnect)
- #   rsconnect::deployApp("C:/Users/debora.bastos/OneDrive - mtegovbr/Documentos/r_studies/shiny")
+#   rsconnect::deployApp("C:/Users/debora.bastos/OneDrive - mtegovbr/Documentos/r_studies/shiny")
 
 # Mais informações
 # https://shiny.posit.co/r/articles/share/shinyapps/?_gl=1*sbyzgp*_ga*MTY5MDQxMDMzNy4xNzY0MzU3Nzg2*_ga_2C0WZ1JHG0*czE3NjQzNTc3ODYkbzEkZzEkdDE3NjQzNTc4MDgkajM4JGwwJGgw
